d64bdf9adbd7ca0c578f985c5646d39b91d30743
sanjeev <sanjeev@rudrainnovative.in>
2023-12-19 19:14:16 +0530

    new way to add type added

--- react/src/components/childrenParent/Parents.tsx
+++ react/src/components/childrenParent/Parents.tsx
@@ -1,9 +1,38 @@
 import React from "react";
+type childrenProps = {
+  children: React.ReactNode;
+  parentName: string;
+  address: {
+    city: string;
+    state: string;
+    pincode: string;
+  };
+  likes: {
+    name: string;
+    type: string;
+  }[];
+  styles: React.CSSProperties; // now we can't give invalid css properties
+};
 
-const Parents = ({ children }: { children: React.ReactNode }) => {
+// const Parents = ({ children }: { children: React.ReactNode }) => {
+const Parents = ({
+  children,
+  parentName,
+  address,
+  likes,
+  styles,
+}: childrenProps) => {
   return (
     <div>
-      <h1>this is parent</h1>
+      <h1 style={styles}>this is parent with name as {parentName}</h1>
+      <span>city: {address.city} </span>
+      <span>state: {address.state} </span>
+      <span>pincode: {address.pincode} </span>
+      {likes.map((like) => (
+        <li>
+          name: {like.name} type: {like.type}
+        </li>
+      ))}
       {children}
     </div>
   );
--- react/src/pages/home/Home.tsx
+++ react/src/pages/home/Home.tsx
@@ -1,23 +1,52 @@
-// import Parents from "../../components/childrenParent/Parents";
+import Parents from "../../components/childrenParent/Parents";
 // import Children from "../../components/childrenParent/Children";
 // import Events from "../../components/eventExample/Events";
 // import Page from "../../components/useStateEx/Page";
 // import Pages from "../../components/useContextEx/Pages";
-import Pages from "../../components/useRefEx/Pages";
+// import Pages from "../../components/useRefEx/Pages";
 // import { ThemeProvider } from "../../context/ThemeContext";
 import "./home.scss";
+import SecondChildren from "../../components/childrenParent/SecondChildren";
 // import SecondChildren from "../../components/childrenParent/SecondChildren";
+
 const Home = () => {
+  const address = {
+    city: "arrah",
+    state: "bihar",
+    pincode: "802352",
+  };
+  const likes = [
+    {
+      name: "cricket",
+      type: "outdoor game",
+    },
+    {
+      name: "carrom",
+      type: "indoor game",
+    },
+  ];
+  const styles = {
+    color: "red",
+    border: "2px solid #333",
+    borderRadius: "10px",
+    padding: "20px 30px",
+  };
   return (
     <div className="home">
       <div className="container">
-        {/* <Parents children={<SecondChildren />} /> */}
+        <Parents
+          styles={styles}
+          children={<SecondChildren />}
+          parentName="godzilla"
+          address={address}
+          likes={likes}
+        />
         {/* <Events /> */}
         {/* <Page /> */}
         {/* <ThemeProvider>
           <Pages />
         </ThemeProvider> */}
-        <Pages />
+        {/* <Pages /> */}
       </div>
     </div>
   );
